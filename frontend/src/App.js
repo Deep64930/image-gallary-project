5881808ee2585a318f449f3cbb13baf549d5ea5e
Deep64930 <deepakraj5455@gmail.com>
2022-11-05 19:35:17 +0530

    Search bar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 
-
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-     <h1>hello world</h1>
-    </div>
+    <>
+     <Header />
+     <Search />
+    </>
   );
 }
 
