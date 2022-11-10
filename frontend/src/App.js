5c4bf5f9d5fd80ad9b698f36313c1f9ee22b2acd
Deep64930 <deepakraj5455@gmail.com>
2022-11-10 18:48:35 +0530

    Adding event

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Search from './components/Search';
-
+import {useState} from 'react';
 
 const handleSearchSubmit =(e)=>{
   e.preventDefault();
@@ -10,11 +10,15 @@ const handleSearchSubmit =(e)=>{
 }
 
 
+
+
 function App() {
+  const [word,setWord]=useState('');
+ 
   return (
     <>
      <Header />
-     <Search handleSubmit={handleSearchSubmit} />
+     <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </>
   );
 }
--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -2,21 +2,29 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
-const Search = ({ handleSubmit }) => {
+const Search = ({ word, setWord, handleSubmit }) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
-        <Form className="d-flex" onSubmit={handleSubmit}>
-          <Form.Control
-            type="search"
-            placeholder="Search for New Images..."
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Search</Button>
-        </Form>
+        <Col xs={12} md={8} lg={6}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Row>
+              <Col xs={9}>
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for new Images..."
+                />
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit"></Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Col>
       </Row>
     </Container>
   );
